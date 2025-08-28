736ea8737ce5c31d4dde920ef9bacf6193836262
huron <aarougaldet@gmail.com>
2025-08-27 22:17:57 -0600

    cses

---
+++ cses/rangue_queries/Static_Range_Sum_Queries.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define sz(x) int((x).size())
+
+vector<ll> tree;
+ll query(int node, int l, int h, int q_l, int q_h){
+    if(l >= q_l && h <= q_h){
+        //cout << "node: " << node << " q_l: " << q_l << " q_h: " << q_h << " tree[node]: " << tree[node] << endl; 
+        return tree[node];
+    }
+    if(h < q_l || l > q_h){
+        return 0;
+    }
+    return query(2 * node, l, (l + h) / 2, q_l, q_h)
+        + query(2 * node + 1, (l + h) / 2 + 1, h, q_l, q_h);
+}
+
+void solve(int testcase){
+    int n, q; cin >> n >> q;
+    vector<int> A(n);
+    for(auto &a : A) cin >> a;
+
+    n = 1;
+    while(n < (int)sz(A)) n <<= 1;
+
+    tree.resize(2 * n);
+    for(int i = 0; i < sz(A); i++){
+        tree[n + i] = A[i];
+    }
+    for(int i = n - 1; i >= 1; i--){
+        tree[i] = tree[2 * i] + tree[2 * i + 1];
+    }
+
+    for(int i = 0; i<q; i++){
+        int a, b; 
+        cin >> a >> b;
+        a--; b--;
+        cout << query(1, 0, n - 1, a, b) << endl;
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+
+    int t = 1;
+    //cin >> t;
+    for(int i = 0; i<t; i++){
+        solve(i);
+    }
+}
