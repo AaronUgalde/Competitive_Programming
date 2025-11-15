83f12f9faa43cf9a00b869a662d3eb5856e1cc41
huron <aarougaldet@gmail.com>
2025-11-15 12:28:36 -0600

    practice

--- codeforces/A_Dasha_and_Stairs.cpp
+++ codeforces/A_Dasha_and_Stairs.cpp
@@ -8,14 +8,15 @@ using ull = unsigned long long;
 #define dbg(x) (cerr << #x << " = " << (x) << '\n')
 
 void solve(int testcase){
-    
+    int a, b; cin >> a >> b;
+    cout << (abs(a - b) >= 2 or (b == 0 and a == 0)? "NO" : "YES") << endl;
 }
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(nullptr);
 
     int t = 1;
-    cin >> t;
+    //cin >> t;
     for(int i = 0; i<t; i++){
         solve(i);
     }
